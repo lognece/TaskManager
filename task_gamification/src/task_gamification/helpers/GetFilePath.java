58e548954bc6c8d1554ab45b35ba43dc6a3b705a
Mariia <damby.m@gmail.com>
2024-01-21 15:45:49 +0100

    added new csv files to GetFilePath

--- task_gamification/src/task_gamification/helpers/GetFilePath.java
+++ task_gamification/src/task_gamification/helpers/GetFilePath.java
@@ -6,6 +6,9 @@ public class GetFilePath {
 
 	public static final String USER_FILE_PATH = "src/users.csv";
 	public static final String TASK_FILE_PATH = "src/tasks.csv";
+	public static final String LEVEL_FILE_PATH = "src/level.csv";
+	public static final String STORY_FILE_PATH = "src/story.csv";
+	public static final String CHARACTER_FILE_PATH = "src/characters.csv";
 	public static final String CHAR_1_FILE_PATH = "src/icon/tiefling.png";
 	public static final String CHAR_2_FILE_PATH = "src/icon/dragonborn.png";
 }
