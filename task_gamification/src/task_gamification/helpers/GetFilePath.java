4ff297025db017d2843fd657793bd275066b78a0
Mariia <damby.m@gmail.com>
2024-01-27 19:15:16 +0100

    moved .csv files into a src/csv_files directory

--- task_gamification/src/task_gamification/helpers/GetFilePath.java
+++ task_gamification/src/task_gamification/helpers/GetFilePath.java
@@ -7,12 +7,12 @@ public class GetFilePath {
 
 	// stores path to file that store user data and tasks data
 
-	public static final String USER_FILE_PATH = "src/users.csv";
-	public static final String TASK_FILE_PATH = "src/tasks.csv";
-	public static final String LEVEL_FILE_PATH = "src/level.csv";
-	public static final String STORY_FILE_PATH = "src/story.csv";
-	public static final String CHARACTER_FILE_PATH = "src/characters.csv";
-	public static final String ABOUT_FILE_PATH = "src/about.csv";
+	public static final String USER_FILE_PATH = "src/csv_files/users.csv";
+	public static final String TASK_FILE_PATH = "src/csv_files/tasks.csv";
+	public static final String LEVEL_FILE_PATH = "src/csv_files/level.csv";
+	public static final String STORY_FILE_PATH = "src/csv_files/story.csv";
+	public static final String CHARACTER_FILE_PATH = "src/csv_files/characters.csv";
+	public static final String ABOUT_FILE_PATH = "src/csv_files/about.csv";
 	public static final String CHAR_1_FILE_PATH = "src/icon/tiefling.png";
 	public static final String CHAR_2_FILE_PATH = "src/icon/dragonborn.png";
 }
