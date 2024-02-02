02013da405b41e868b47bd805be8ff6821a6fa76
Mariia <damby.m@gmail.com>
2024-02-02 01:58:25 +0100

    added test for CheckLevelChange()

---
+++ task_gamification/src/tests/testCheckLevelChange.java
@@ -0,0 +1,60 @@
+package tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import task_gamification.entity.User;
+import task_gamification.task_manager.Score;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class testCheckLevelChange {
+
+	private Score score;
+	private User loggedInUser;
+
+	@TempDir
+	Path tempDir;
+
+	@BeforeEach
+	void setUp() throws IOException {
+		score = new Score();
+		loggedInUser = new User();
+
+		File testFile = tempDir.resolve("Test.csv").toFile();
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(testFile))) {
+			writer.write("Tom;0;110;2;2024-01-26;[T, o, m, T, o, m];tom@tom.com\n");
+		}
+	}
+
+	/**
+	 * checkLevelChange returns false when newScore is less than nextLevelXP
+	 *
+	 */
+	@Test
+	void levelChangeNoUpdate() {
+		int newScore = 10;
+		boolean result = score.checkLevelChange(String.valueOf(loggedInUser), newScore);
+		assertFalse(result);
+
+	}
+
+	/**
+	 *  if newScore is equal or greater than nextLevelXP
+	 *  checkLevelChange returns true and userLevel is updated
+	 */
+	@Test
+	void levelChangeWithUpdate() {
+		int newScore = 150;
+		boolean result = score.checkLevelChange(String.valueOf(loggedInUser), newScore);
+		assertTrue(result);
+
+	}
+}
